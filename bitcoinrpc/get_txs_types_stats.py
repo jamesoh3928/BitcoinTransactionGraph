2091cc19ae31eea48208cc1faadb39c284d9cdfb
James Oh <jo9347@cs.rit.edu>
2023-04-10 06:06:01 -0400

    Update on get_txs_types_stats

--- bitcoinrpc/get_txs_types_stats.py
+++ bitcoinrpc/get_txs_types_stats.py
@@ -61,6 +61,15 @@ def write_tx_types_data(block_hashes, start_height, end_height):
                     data_by_date[block_time] = {}
                     data_by_date[block_time][script_type] = 1
 
+    # Write down last day
+    with open(f'./txs_types_stats/{start_height}_to_{end_height}.csv', 'a') as f1:
+        f1.write(cur_date + ', ')
+        for script_type in data_by_date[cur_date]:
+            # Write type
+            f1.write(script_type + ': ')
+            f1.write(str(data_by_date[cur_date][script_type]) + ', ')
+        f1.write('\n')
+
     # pubkey, witness_v0_keyhash: 4763, nulldata: 22, pubkeyhash: 2885, scripthash: 4480, witness_v0_scripthash: 663, witness_v1_taproot: 52
     # with open(f'./txs_types_stats/{start_height}_to_{end_height}.csv', 'w') as f1:
     #     for date in data_by_date:
